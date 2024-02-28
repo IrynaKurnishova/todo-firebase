122ba838f497137ff685b31d926785eea8f71150
irinakurnishova <kurnishovaiv@gmail.com>
2024-02-28 14:43:45 +0100

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLzLhCBTvweR8T-HkefajD85r20KAbrYk",
+    authDomain: "to-do-app-1228b.firebaseapp.com",
+    projectId: "to-do-app-1228b",
+    storageBucket: "to-do-app-1228b.appspot.com",
+    messagingSenderId: "121950034501",
+    appId: "1:121950034501:web:4c6b32caa78d2cf09c612c",
+    measurementId: "G-Z3ZW4SXTEH"
+};
